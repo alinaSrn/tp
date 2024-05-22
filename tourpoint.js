cf2dd54e79943862cdbe06fafcdabde9df6699a9
alinaSrn <99744478+alinaSrn@users.noreply.github.com>
2024-05-22 15:54:17 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,158 @@
+var tourpoint = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Patong Beach",
+          "type": "Natural",
+          "level": 4.3
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            98.29544196395341,
+            7.896571113254012
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Central Phuket Festival",
+          "type": "Special",
+          "level": 4.6
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            98.36785897813843,
+            7.891890141483941
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Jungceylon",
+          "type": "Natural",
+          "level": 4.3
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            98.29932527116591,
+            7.891362118292946
+          ]
+        },
+        "id": 2
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Wat Chalong Temple",
+          "type": "Cutural",
+          "level": 4.6
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            98.33612555710698,
+            7.846340782620089
+          ]
+        },
+        "id": 3
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Chino-Portuguese",
+          "type": "Cutural",
+          "level": "5.0"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            98.38752653714329,
+            7.886222329020185
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Khao Rang",
+          "type": "Natural",
+          "level": 3.8
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            98.3797725584447,
+            7.8938480179976835
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Kathu Shrine",
+          "type": "Cutural",
+          "level": 4.5
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            98.34507335581952,
+            7.916409120227883
+          ]
+        },
+        "id": 6
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Maikhao Beach",
+          "type": "Natural",
+          "level": 4.6
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            98.29947907013013,
+            8.131852477055347
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Sarasin Bridge ",
+          "type": "Special",
+          "level": 4.5
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            98.29810852028038,
+            8.202098629206034
+          ]
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "Phromthep Cape",
+          "type": "Natural",
+          "level": 4.7
+        },
+        "geometry": {
+          "coordinates": [
+            98.30529598542273,
+            7.7622912299210896
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
